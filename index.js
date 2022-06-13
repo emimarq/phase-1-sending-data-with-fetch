20b9835d8376cb0320e50c9b1f471bcc0537696b
Emilio <marqetm@gmail.com>
2022-06-12 21:12:20 -0500

    commit tree b328ad82fa1ea52a3b56f6444b3acd2e52c60751

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (object) {
+      const credentials = document.createElement('h1')
+      credentials.innerHTML = `${object["id"]}`
+      document.body.append(credentials)
+    })
+    .catch(function (error) {
+      const errorMsg = document.createElement('h1')
+      errorMsg.innerHTML = error.message
+      document.body.append(errorMsg)
+    })
+}
+
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json()
+//   })
+//   .then(function (object) {
+//     console.log(object)
+//   })
+//   .catch(function (error) {
+//     document.body.style.background = "#050505"
+//     setTimeout(() => {
+//       alert("404: Nothing was found")
+//     }, 50)
+//   })
